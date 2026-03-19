2e359663d98bcc0ea414e48c3b745f0759d87e61
Nambooproo <nomorehopenam@gmail.com>
2026-03-19 17:52:30 +0000

    Yes

--- index.ts
+++ index.ts
@@ -3,35 +3,33 @@ import express from "express";
 import * as Discord from "discord.js";
 
 const TOKEN = process.env.DISCORD_TOKEN;
+
 const app = express();
 
 const client = new Discord.Client({
   intents: [
     Discord.GatewayIntentBits.Guilds,
     Discord.GatewayIntentBits.GuildMessages,
-  Discord.GatewayIntentBits.MessageContent
+    Discord.GatewayIntentBits.MessageContent
   ]
 });
-
-app.use(express.json());
-
 
 app.get("/", (_, res) => {
   res.send("Bot is Alive");
 });
 
 app.listen(process.env.PORT || 3000, () => {
-  console.log("bot is running!");
+  console.log("Web server running");
 });
 
-client.on(Discord.Events.MessageCreate, async (message: Discord.Message) => {
+client.once(Discord.Events.ClientReady, () => {
+  console.log("✅ Bot is ONLINE");
+});
+
+client.on(Discord.Events.MessageCreate, async (message) => {
   if (message.content === "ping") {
     await message.reply("pong");
   }
 });
 
-try {
-  client.login(TOKEN);
-} catch (err) {
-  console.error(err);
-}
+client.login(TOKEN).catch(console.error);
